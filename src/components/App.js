0eff7333b0356dad6c02b50181a428cd3993fb6c
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-09-02 13:28:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,58 @@
 
 import React from "react";
 import './../styles/App.css';
+import {useState} from "react";
 
 const App = () => {
+  const [fields, setFields] = useState([{ name: "", age: "" }]);
+
+  function addField() {
+    setFields((fields) => [...fields, { name: "", age: "" }]);
+  }
+  function removeField(index, e) {
+    e.stopPropagation();
+    const values = [...fields];
+    values.splice(index, 1);
+    setFields(values);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(fields);
+  }
+  function handleChange(e, index) {
+    const values = [...fields];
+    values[index][e.target.name] = e.target.value;
+    setFields(values);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form onSubmit={handleSubmit}>
+        {fields.map((field, index) => (
+          <div key={index}>
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={field.name}
+              onChange={(e) => handleChange(e, index)}
+            />
+            <input
+              type="number"
+              placeholder="age"
+              name="age"
+              value={field.age}
+              onChange={(e) => handleChange(e, index)}
+            />
+            <button type="button" onClick={(e) => removeField(index, e)}>
+              remove
+            </button>
+          </div>
+        ))}
+        <button type="submit">Submit</button>
+      </form>
+      <button onClick={addField}>...Add more</button>
     </div>
-  )
+  );
 }
 
 export default App
